f916cbdc3de2089c97be4655c7056310ccf7de2b
dickyzuerst <dzuerst@gmail.com>
2024-11-16 22:11:37 +0700

    change patients fk to users

--- laravel-web-api/database/migrations/2024_11_14_193602_create_owners_table.php
+++ laravel-web-api/database/migrations/2024_11_14_193602_create_owners_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('email');
             $table->string('name');
             $table->string('phone');
